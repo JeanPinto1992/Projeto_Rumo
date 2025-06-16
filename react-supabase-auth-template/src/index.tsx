cef7ba10212da784735c37b1cf2c3785ed210eb2
JeanPinto1992 <sanchesjean@gmail.com>
2025-06-16 18:12:52 -0300

    Remove arquivos antigos de index e adiciona verificação de elemento raiz no novo index.tsx

--- react-supabase-auth-template/src/index.tsx
+++ react-supabase-auth-template/src/index.tsx
@@ -31,7 +31,8 @@ const theme = createTheme({
 });
 
 const container = document.getElementById('root');
-const root = createRoot(container!);
+if (!container) throw new Error('Failed to find the root element');
+const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
